ed616348b305ac695c547c97ac7eca4e88fe6616
Chris <rasch83@gmail.com>
2020-11-05 12:48:19 -0600

    added markdown images and image in bio.js

--- src/data/welcome.md
+++ src/data/welcome.md
@@ -4,6 +4,8 @@ data: "2020-10-13"
 ---
 
 Welcome Home!
+<!-- Added an image here -->
+![A Test Image](../images/gatsby-icon.png)
 
 Here is a welcome video to welcome you to my website:
 <iframe width="560" height="315" src="https://www.youtube.com/embed/7nH2NYpeKa4" frameborder="0" allow="accelorometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
--- src/pages/bio.js
+++ src/pages/bio.js
@@ -9,12 +9,13 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import People from "../components/people"
+import myImg from "../images/gatsby-astronaut.png"
 
 const Bio = () => {
     return (
         <Layout>
             <SEO title="Bio" />
-            <People avatar="https://source.unsplash.com/random/" title="Chris" excerpt="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ex asperiores cumque atque quaerat labore dolorum, placeat natus velit omnis voluptatum voluptatem deleniti fugiat rem aliquam culpa, illo voluptas, laboriosam ad blanditiis quasi sapiente? Soluta praesentium iure cupiditate voluptates? Esse maxime enim sequi aspernatur possimus quia sit similique odit laudantium." />
+            <People avatar={myImg} title="Chris" excerpt="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ex asperiores cumque atque quaerat labore dolorum, placeat natus velit omnis voluptatum voluptatem deleniti fugiat rem aliquam culpa, illo voluptas, laboriosam ad blanditiis quasi sapiente? Soluta praesentium iure cupiditate voluptates? Esse maxime enim sequi aspernatur possimus quia sit similique odit laudantium." />
             <People avatar="https://source.unsplash.com/random/" title="Sue" excerpt="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ex asperiores cumque atque quaerat labore dolorum, placeat natus velit omnis voluptatum voluptatem deleniti fugiat rem aliquam culpa, illo voluptas, laboriosam ad blanditiis quasi sapiente? Soluta praesentium iure cupiditate voluptates? Esse maxime enim sequi aspernatur possimus quia sit similique odit laudantium." />
             <People avatar="https://source.unsplash.com/random/" title="Matt" excerpt="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ex asperiores cumque atque quaerat labore dolorum, placeat natus velit omnis voluptatum voluptatem deleniti fugiat rem aliquam culpa, illo voluptas, laboriosam ad blanditiis quasi sapiente? Soluta praesentium iure cupiditate voluptates? Esse maxime enim sequi aspernatur possimus quia sit similique odit laudantium." />
             <Link to="/">Back to Home</Link>
